5479f5aee0331512a88059eeb02e954f5db89241
Mustapha-Nawawi-T <85850362+Mustapha-Nawawi-T@users.noreply.github.com>
2022-03-08 21:11:37 +0100

    first Hello world in c++

---
+++ cpp_Module_00/ex00/megaphone.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mannouao <mannouao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 16:48:22 by mannouao          #+#    #+#             */
+/*   Updated: 2022/03/08 18:39:07 by mannouao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <string>
+# include <iostream>
+
+int main()
+{
+	std::string str_1 = "one";
+
+	std::cout << str_1.size()<< std::endl;
+	// if (ac < 2)
+	// {
+	// 	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	// 	return (1);
+	// }
+	// for (int i = 1; i < ac; i++)
+	// {
+	// 	for (int j = 0; j < std::strlen(av[i]); j++)
+	// 	{
+	// 		if ((av[i][j] >= 'a' && av[i][j] <= 'z') || (av[i][j] >= 'A' && av[i][j] <= 'Z'))
+	// 			std::cout << std::toupper(av[i][j]);
+	// 		else
+	// 			std::cout << av[i][j];
+	// 	}
+	// 	if (av[i + 1])
+	// 		std::cout << " ";
+	// }
+	// std::cout << std::endl;
+	// return (0);
+}
