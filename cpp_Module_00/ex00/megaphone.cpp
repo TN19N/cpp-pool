8ec5f8577d82de9ac7d311c828e3404e97b6dbce
Mustapha-Nawawi-T <85850362+Mustapha-Nawawi-T@users.noreply.github.com>
2022-03-09 08:51:21 +0100

    add ex00 cpp_Module_00

--- cpp_Module_00/ex00/megaphone.cpp
+++ cpp_Module_00/ex00/megaphone.cpp
@@ -6,35 +6,30 @@
 /*   By: mannouao <mannouao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 16:48:22 by mannouao          #+#    #+#             */
-/*   Updated: 2022/03/08 18:39:07 by mannouao         ###   ########.fr       */
+/*   Updated: 2022/03/09 08:50:11 by mannouao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <string>
+# include <cctype>
 # include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
-	std::string str_1 = "one";
-
-	std::cout << str_1.size()<< std::endl;
-	// if (ac < 2)
-	// {
-	// 	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	// 	return (1);
-	// }
-	// for (int i = 1; i < ac; i++)
-	// {
-	// 	for (int j = 0; j < std::strlen(av[i]); j++)
-	// 	{
-	// 		if ((av[i][j] >= 'a' && av[i][j] <= 'z') || (av[i][j] >= 'A' && av[i][j] <= 'Z'))
-	// 			std::cout << std::toupper(av[i][j]);
-	// 		else
-	// 			std::cout << av[i][j];
-	// 	}
-	// 	if (av[i + 1])
-	// 		std::cout << " ";
-	// }
-	// std::cout << std::endl;
-	// return (0);
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+	for (int i = 1; argv[i]; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
+		{
+			if (std::islower(argv[i][j]))
+				std::cout << (char)std::toupper(argv[i][j]);
+			else
+				std::cout << argv[i][j];
+		}
+	}
+	std::cout << std::endl;
+	return (0);
 }
