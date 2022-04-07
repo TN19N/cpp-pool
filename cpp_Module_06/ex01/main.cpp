6aa7abc2610601ca8e44a784904c33bb1bc7a169
Mustapha-Nawawi-T <85850362+Mustapha-Nawawi-T@users.noreply.github.com>
2022-04-07 22:59:12 +0000

    finish Module_06

--- cpp_Module_06/ex01/main.cpp
+++ cpp_Module_06/ex01/main.cpp
@@ -6,11 +6,66 @@
 /*   By: mannouao <mannouao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 17:56:18 by mannouao          #+#    #+#             */
-/*   Updated: 2022/04/07 17:56:26 by mannouao         ###   ########.fr       */
+/*   Updated: 2022/04/07 20:55:05 by mannouao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include <iostream>
+
+struct Data
+{
+	unsigned char a;
+	unsigned char b;
+	unsigned char c;
+	unsigned char d;
+	
+	bool operator == (const Data& other)
+	{
+		if (
+			 this->a == other.a &&
+			 this->b == other.b &&
+			 this->c == other.c &&
+			 this->d == other.d
+		   )
+		   return (true);
+		else
+			return (false);
+	}
+};
+
+
+uintptr_t serialize(Data* ptr)
+{
+	return (*reinterpret_cast<uintptr_t*> (ptr));
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	uintptr_t *ptr = new uintptr_t(raw);
+	return (reinterpret_cast<Data*> (ptr));
+}
+
 int main()
 {
+	Data *o_d = new Data();
 	
+	uintptr_t tmp = serialize(o_d);
+	Data *t_d = deserialize(tmp);
+	
+	if (*t_d == *o_d)
+		std::cout << "equal" << std::endl;
+	else
+		std::cout << "not equal" << std::endl;
+
+	// Data *d = deserialize(1337);
+
+	// std::cout << (int)d->a << " - " << (int)d->b << " - " << (int)d->c << " - " << (int)d->d << std::endl;
+	
+	// uintptr_t tmp = serialize(d);
+
+	// std::cout << " > " << tmp << " < " << std::endl;
+
+	delete t_d;
+	delete o_d;
+	// delete d
 }
---
+++ cpp_Module_06/ex02/main.cpp
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mannouao <mannouao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 20:56:50 by mannouao          #+#    #+#             */
+/*   Updated: 2022/04/07 22:53:34 by mannouao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+# include <ctime>
+
+class Base
+{
+	public:
+		virtual ~Base(void) {};	
+};
+
+class A : public Base
+{	
+};
+
+class B : public Base
+{	
+};
+
+class C : public Base
+{	
+};
+
+Base *generate(void)
+{
+	srand(time(NULL));
+
+	if (rand() % 3 == 2)
+		return (new A());
+	else if (rand() % 3)
+		return (new B());
+	else
+		return (new C());
+}
+
+void identify(Base* p)
+{
+	if (dynamic_cast<A*> (p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*> (p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*> (p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Base" << std::endl; 
+}
+
+void identify(Base& p)
+{
+	try
+	{
+		(void)dynamic_cast<A&> (p);
+		std::cout << "A" << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		try
+		{
+			(void)dynamic_cast<B&> (p);
+			std::cout << "B" << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			try
+			{
+				(void)dynamic_cast<C&> (p);
+				std::cout << "C" << std::endl;
+			}
+			catch(const std::exception& e)
+			{
+				std::cout << "Base" << std::endl;
+			}
+		}
+	}
+}
+
+int main()
+{
+	Base *abc = generate();
+
+	identify(abc);
+	identify(*abc);
+
+	delete abc;
+}
